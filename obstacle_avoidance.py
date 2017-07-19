c3e198dcb354182d8c1f5cf44141e32d6f22e1ec
leoybkim <leoybkim@gmail.com>
2017-07-18 22:01:46 -0400

    optimize loops using numpy

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -21,25 +21,27 @@ while (cap.isOpened()):
         canny2, contours, hierarchy = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
         # Fill from bottom side
-        # h, w = frame.shape[:2]
-        # row_inds = np.indices((h, w))[0] # gives row indices in shape of img
-        # row_inds_at_edges = row_inds.copy()
-        # row_inds_at_edges[contours==0] = 0 # only get indices at edges, 0 elsewhere
-        # max_row_inds = np.amax(row_inds_at_edges, axis=0) # find the max row ind over each col
-        #
-        # inds_after_edges = row_inds >= max_row_inds
-        #
-        # filled_from_bottom = np.zeros((h, w))
-        # filled_from_bottom[inds_after_edges] = 255
 
         h, w = canny2.shape[:2]
+
+        # filled_from_bottom = np.zeros((h, w))
+        # for col in range(w):
+        #     for row in reversed(range(h)):
+        #         if canny2[row][col] < 255:
+        #             filled_from_bottom[row][col] = 255
+        #         else:
+        #             break
+
+        # For each column, find the maximum row index:
+        # 1) Reverse the array vertically
+        # 2) Use np.argmax to return the first instance of the maximum in an array
+        max_row_index = h - np.argmax(canny2[::-1], axis=0)
+        row_index = np.indices((h, w))[0]
+
+        index_after_edges = row_index >= max_row_index
+
         filled_from_bottom = np.zeros((h, w))
-        for col in range(w):
-            for row in reversed(range(h)):
-                if canny2[row][col] < 255:
-                    filled_from_bottom[row][col] = 255
-                else:
-                    break
+        filled_from_bottom[index_after_edges] = 255
 
         pixels = np.argwhere(filled_from_bottom == 255)
 
